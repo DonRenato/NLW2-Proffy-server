8ea647bc6727736a1163072eef47662bdd19e1aa
Renato  Avellar <onizuka@Mac-mini.local>
2020-08-04 19:34:15 -0300

    recriando db

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,38 @@
 import express from 'express';
+import db from './database/connection';
 
 const routes = express.Router();
 
 
-routes.get('/', (req, res)=>{
+routes.post('/classes', async (req, res)=>{
+    const {
+        name,
+        avatar,
+        whatsapp,
+        bio,
+        subject,
+        cost,
+        schedule
+    } = req.body;
 
-    return res.send("Hello World!!!!!");
+    const insertedUsersId = await db('users').insert({name,
+        avatar,
+        whatsapp,
+        bio
+    });
+
+    const user_Id = insertedUsersId[0];
+
+    await db('classes').insert({
+        subject,
+        cost,
+        user_Id
+    });
+
+    return res.send();
 })
-
+ 
 
 export default routes;
+
+
