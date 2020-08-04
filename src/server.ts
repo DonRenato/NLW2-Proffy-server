53a2f3b38fa2da24361caf1b8a0fdc611a99670d
Renato  Avellar <onizuka@Mac-mini.local>
2020-08-04 16:24:10 -0300

    testando rotas

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,15 @@ import express from 'express';
 
 const app = express();
 
+app.get('/users', (req, res)=>{
+    const users = [
+        {name: "Seo Dartinho", age: 33},
+        {name: "Onizuka", age: 33}
+    ]
+    return res.json(users);
+})
 //localhost:3333
 app.listen(3333);
+
+
 
